57c880423606c60da641f9e29f0477fde0bf2618
lw <lw_git@mail.ru>
2020-07-15 11:31:21 +0500

    Placed buttons on canvas

--- clock.py
+++ clock.py
@@ -19,57 +19,108 @@ class Application(tk.Frame):
         self.id3 = 0
         self.id4 = 0
         self.oval = 0
-        self.x = 250
-        self.y = 200
         self.clock_size = 170
         self.time = datetime.datetime.now()
         self.sound = False
         self.alarm = False
         self.is_alarm = False
+        self.panel = 55        
 
         # ------------------Widgets------------------------------
-        self.canvas = tk.Canvas(root, width=500, height=400, bg="white")
+        self.canvas = tk.Canvas(root, width=self.clock_size * 2.5,
+                                height=self.clock_size * 2.5 + self.panel,
+                                bg="white")
         self.canvas.bind("<Button-1>", self.alarm_arrow)
         self.canvas.pack()
+        self.calculate_center()
 
-        self.sound_button = tk.Button(
-            text='Sound On',
-            font='Consolas 9', bg='white', command=self.toggle_sound)
+        self.canvas.mute = tk.PhotoImage(data='''R0lGODlhMgAyAPAAAAAAAAAAACH5BAEAAAAALAAAAAAyADIAAALVhI+py+0Po5y02ouz3lyFH3QZ+IkXWTqgeaDpQjZv5YaJy6An3vKeP6kZaracjsYTqo4UJVAWQx6ZwxUiCo06rT1udXbbTs
+        FAbJj41RavZnMXDXAaue40XJlde99EG37OBhbXp0b181JHOGYYWJSoWFjXFXgImfL3Rnlmefk0iLj3ydlpiPU4avXndYpKGqm52eqXpBcqKjs71TiHy0dm6zvaSCcJARnrOkJYmbqG0QfjqQHH
+        LDgtbWd9DZztzFEczPJpzCi+xG0erZ3O3u7+Dh8vr1EAADs=''')
 
-        self.alarm_button = tk.Button(
-            text='Alarm On',
-            font='Consolas 9', bg='white', command=self.toggle_alarm)
+        self.canvas.sound = tk.PhotoImage(data='''R0lGODlhMgAyAPZjAAEBAQICAgMDAwQEBAUFBQYGBggICAkJCQoKCgsLCwwMDA0NDQ4ODg8PDxAQ\nEBERERISEhQUFBcXFxsbGxwcHCAgICMjIyUlJSgoKCkpKSoqKisrKywsLC0tLS4uLjU1NT
+        k5OURE\nREZGRkpKSk9PT1NTU1dXV1hYWF5eXmNjY2ZmZmdnZ2hoaGlpaWpqam1tbW5ubnFxcXZ2dn5+foSE\nhIqKio2NjZOTk5aWlpmZmZqamqOjo6ampqioqKysrLCwsLe3t7u7u7+/v8HBwcXF
+        xcrKysvLy8zM\nzM3Nzc7Ozs/Pz9TU1NjY2NnZ2dra2t/f3+Hh4eLi4ufn5+jo6Onp6evr6+7u7vHx8fPz8/T09PX1\n9fb29vf39/n5+fr6+vv7+/z8/P39/f7+/v///wAAAAAAAAAAAAAAAAAAAA
+        AAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5\nBAAAAAAALAAAAAAyADIAAAf+gGOCg4SFhoeIiYqLjI2Oj5CRkpOU
+        lZaXmJmamFtEjlubhTkPDwSL\nUKQtoYIapKSLLa5doRSur4uuC5sLtreKW64gmby9D4dBC6aCF66XwMW+hAnNgq4alUDQ1IUWsoIl\n25Eq2uGEpAsJg64ykhLk5YNMrqBjH/A4Ifn6+8TvxmNVSP
+        EYNO0BAEHP/g0y4a/hLXu+hmwjpmpQ\nB4cNBXVx9UIdqR6CJpwjdBHju0GtHugSBIDaEl4rBZU0qc2cryMjq0WbSbOYzQc1PCqcZ7EntJhj\nWj64pnMoqSdFjfocRIKUsjFExxA7ElWqLUI2SKVr
+        ekVQQSFdvUaLYVUoPWKvRdKqJaR0gtBZWEkx\nkeuVEDF2TYWWlan2q1tBUnLmVTiG51xBI7YpHWslnOO+TS0IpUH4wVhBbAvfkrEtoC9iIQoducG6\nteuC/gRV4aViELHP3iDBJpfIFdQx46I9Ch
+        H7ELGYxJhK2nHSEENSXMf4gAdJXk1DBa8atnS90A+V\ng1BQn7RbOCJXBzUR6MWo3yqei0YJXDVmRltFiQ3Sl92RUZfo+wUo4IAEFmjggfQFAgA7'
+        ''')
 
-        self.sound_button.pack()
-        self.alarm_button.pack()
+        self.canvas.alarm_off = tk.PhotoImage(data='''R0lGODlhMgAyAPeRABQUFBcXFxgYGBkZGRoaGhsbGxwcHB0dHR4eHh8fHyAgICEhISIiIiMjIyQk\nJCUlJSYmJicnJygoKCkpKSsrKywsLC0tLS4uLi8vLzIyMjMzMzc3Nzg4ODk5OTo6Ojw8PD
+        4+PkBA\nQEZGRkdHR0hISEpKSktLS01NTU9PT1BQUFJSUlVVVVZWVldXV1hYWFlZWVpaWltbW15eXmBgYGJi\nYmdnZ2hoaGtra21tbW9vb3BwcHFxcXJycnd3d3l5eXp6ent7e4CAgIGBgYKCgoOD
+        g4SEhIWFhYaG\nhoeHh4mJiYqKio2NjY6Ojo+Pj5CQkJGRkZaWlpeXl5iYmJ2dnZ+fn6GhoaKioqSkpKWlpa2tra6u\nrrCwsLGxsbKysrOzs7W1tbe3t7m5ub29vb6+vr+/v8DAwMHBwcXFxcbGxs
+        jIyMnJycrKys7Ozs/P\nz9DQ0NLS0tTU1NXV1dbW1tfX19nZ2dzc3N3d3d7e3uDg4OHh4eLi4uPj4+bm5ufn5+jo6Onp6erq\n6uzs7O3t7e7u7u/v7/Dw8PHx8fLy8vT09PX19fb29vf39/r6+vv7
+        +/z8/P39/f7+/v///wAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        AAAAAAAAAAAAAAAAAAACH5BAAAAAAALAAAAAAyADIA\nAAj+ACMJHEiwoMGDCBMqXMiwocOHBxsZlBiJIsSGFgVmXLjx4sRIhJCwkIElYQ4VJwh19GgQDwYG\nDx4YkNGRigEDUBqtZEkQBoOfDxgY
+        qFIQD8wTPCNWrNgoQMygQWFoFHjiJ56kC+kYgArzAQhEA6Hc\nJIoVoRYQBLrGhMmAgIw4dgwwkFrWICIYW4OyVRuTgAeYV+sOlEiC7dPDP9US0CK4oJCtahMfXsuA\nRmOCf/IaRrz3gQRDZZY2Tg
+        J0LeKnPw18wUPgzeVGHkx3LR3ZRqSqQnZ6NJQWqmnKejEYEvuAxGUy\nBHwbjvyAQBk7QA1chsJ3tt6YBnJEEtE1wJ/GQjr++0asodETzQbUNM7BnGtXAmroJO9qgHFZiTC4\nTt7rIxII2gxI0Vhh
+        qEm21gaNIJGXXgww0Vhs4/1GQBtvaMYVEo0tZ11QQkTyl2wx7XCfQE7xRRkI\nkfhg4HUsCGZHcsrRF4ca840HkweCcSGXfkEZwEQjGjCnmG4PhVddTCJEwsOOv+k1YV0kcIadHWUs\nuBxqDmJFCI
+        ybGSCFIRqgptxhxnmkk0CkNckAUjbQJuZ1BMRBZEKN/HGaXniEweR1wLHVIkQUNVKV\nm0MZIsFmb4rpo0d4EHjdTy3SoCGIQL3nw5wEMWHlb31wAeObJpYWFAZoNNQHl8DVZyeIBY7JoFeO\nDZGx
+        o4EMyBCJT3slliuDosrUEB1CICFEsArG9IcdQAwrLBLLBquss8w666xoDyEh1BeXedRGWjUQ\n9EYAuur6QLYa/YWBShI1wlqlqG1AboptkUGQREGK+RMPjVFURgAEaGeQp+559p1gFKHxBReGIARF
+        \nb0FJ0Ma7U30UCR5M2MADFWdCrPHGHHfsMUsBAQA7''')
+
+        self.canvas.alarm_on = tk.PhotoImage(data='''R0lGODlhMgAyAPZiABcXFxgYGBkZGRoaGhsbGxwcHB0dHR4eHh8fHyAgICEhISIiIiMjIyQkJCUl\nJScnJzIyMjMzMzQ0NDY2Njc3Nzw8PD4+PkFBQUJCQkVFRVBQUFNTU11dXV5eXmBgYGFhYW
+        JiYmZm\nZmhoaGpqam5ubm9vb3BwcHNzc3V1dXd3d3l5eXp6en19fX5+foODg4SEhIWFhYmJiY2NjY6OjpOT\nk5aWlpiYmJqamp2dnaCgoKSkpKampqysrLCwsLGxsbOzs7S0tLW1tba2tri4uL6+
+        vsbGxsfHx8jI\nyMrKys3NzdLS0tPT09TU1NjY2NnZ2dvb29zc3N/f3+Li4uPj4+bm5u3t7e7u7vDw8PLy8vPz8/T0\n9PX19fn5+fr6+vv7+/z8/P39/f7+/v///wAAAAAAAAAAAAAAAAAAAAAAAA
+        AAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5\nBAAAAAAALAAAAAAyADIAAAf+gGKCg4SFhoeIiYqLjI2Oj5CRkFyF
+        lJKXiFMrGyJCmJ+DRwkNDQkD\nLpagk2ITo6QNA0Wqkk0Dr6UNK4Ops4apPa63IZS8vYSWNhMGt68JBiRTxpkXy8ykrgkJPdKFVq3W\n4LCegsWgHMHhzAlU5MZHy+jp1ybcXCLx8q8DW9Jc+PnXck
+        h7BzBcCmk0/hW8IG1FwXAGpHF4CA7A\nFWMZFAIc0MQYBIrWYhnTCNDAuFnVQDYTCIrSFVsqb9HoxQRmTFIsevVIeZNDrxkkAULo9eHmrX2q\nuBi9ZWCbpFQEl5KiB+qe1GtWPimxeRObrkverh49
+        +UjJN7GuDNSAZMJA0JtrBix0ZESEq1hrIRrV\ncHsN1zVsfrM1awY4AUNGRVzAWOxCMWMYjRdLhvzYMWXKim2IKcet86IdbrFlS6DBc6Kd4XyaPnSF\n5ysDOFYjigHvFgXOssWkGDDKmYUouX0JYg
+        KDRAoewZMrX858eSAAOw==''')
+
+        self.btn_sound = self.canvas.create_image(
+            35, self.y * 2 + 30, image=self.canvas.sound)
+        self.btn_alarm = self.canvas.create_image(
+            self.x * 2 - 35, self.y * 2 + 30, image=self.canvas.alarm_on)
+
+        self.canvas.tag_bind(self.btn_sound, "<Button-1>", self.toggle_sound)
+        self.canvas.tag_bind(self.btn_alarm, "<Button-1>", self.toggle_alarm)
+
+        self.canvas.create_rectangle(
+            5, 5, self.x * 2 - 5, self.y * 2 - 5, width=2)
 
         # ---------------First render and start------------------
         self.first_render()
         self.tick()
 
     # ---------------------Service methods--------------------------
+    def calculate_center(self):
+        self.x = int(self.canvas['width']) // 2
+        self.y = (int(self.canvas['height']) - self.panel) // 2
+
     def get_position(self, angle):
         _angle = math.radians(angle)
         x = self.x + self.clock_size * math.cos(_angle)
         y = self.y + self.clock_size * math.sin(_angle)
         return x, y
 
-    def toggle_sound(self):
+    def toggle_sound(self, event):
         self.sound = not self.sound
-        text = 'Sound On' \
-            if self.sound_button['text'] == 'Sound Off' else 'Sound Off'
-        self.sound_button.config(text=text)
+        if self.sound:
+            self.canvas.itemconfig(self.btn_sound, image=self.canvas.mute)
+        else:
+            self.canvas.itemconfig(self.btn_sound, image=self.canvas.sound)
 
-    def toggle_alarm(self):
+    def toggle_alarm(self, event):
         self.alarm = not self.alarm
-        text = 'Alarm On' \
-            if self.alarm_button['text'] == 'Alarm Off' else 'Alarm Off'
-        self.alarm_button.config(text=text)
-        self.is_alarm = False
-        PlaySound(None, SND_PURGE)
+        if self.alarm:
+            self.canvas.itemconfig(self.btn_alarm,
+                                   image=self.canvas.alarm_off)
+        else:
+            self.canvas.itemconfig(self.btn_alarm,
+                                   image=self.canvas.alarm_on)
+        if self.is_alarm:
+            self.is_alarm = False
+            PlaySound(None, SND_PURGE)
 
     # ----------------------Alarm methods---------------------------
     def alarm_arrow(self, event):
+        if event.y >= self.y * 2 - 5:
+            return
         radians = math.atan2(event.y - self.y, event.x - self.x)
         self.angle4 = math.degrees(radians)
         self.canvas.delete(self.id4)
@@ -144,7 +195,6 @@ class Application(tk.Frame):
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Clock')
-    root.geometry('500x460+300+100')
     root.resizable(False, False)
     Application(root)
     root.mainloop()
